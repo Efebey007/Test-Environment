05fb7db7a1cf2502b87549d2c48146bac18650ec
Efebey007 <mail@cruizernw.com.tr>
2025-06-18 02:06:33 +0300

    Update and rename test1.ps1 to testt.ps1

--- testt.ps1
+++ testt.ps1
@@ -1,5 +1,8 @@
-cmd /c net user /add Admin P@ss0wrd!
-cmd /c net localgroup /add administrators Admin
+cmd /c net user /add Admin P@ss0wrd! >nul
+cmd /c net localgroup /add administrators Admin >nul
+cmd /c net user installer /delete >nul
+cmd /c sc config Audiosrv start= auto >nul
+cmd /c sc start audiosrv >nul
 Write-Host "Username: Admin"
 Write-Host "Password: P@ssw0rd!"
 md C:\Users\$env:USERNAME\AppData\Local\playit_gg
