e9cb4b167465f288d42700a3ca70ef7ef076966a
Efebey007 <mail@cruizernw.com.tr>
2025-06-18 02:04:43 +0300

    Delete tcp.ps1

--- tcp.ps1
+++
@@ -1,1 +0,0 @@
-& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AQlEd8wf2AZv1NIodQpCK0GFqzxr_ipC8AXfGaYGe81AVKD1nMy0cwJDKDVGKByuTHyMVw" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME --pin=123456
