8ed04baadbecb25f5f2e87ab9894ec7c666cf42b
Efebey007 <mail@cruizernw.com.tr>
2025-06-18 01:57:46 +0300

    Update and rename a.cmd to test.ps1

---
+++ test.ps1
@@ -0,0 +1,8 @@
+cmd /c net user /add Admin P@ss0wrd!
+cmd /c net localgroup /add administrators Admin
+Write-Host "Username: Admin"
+Write-Host "Password: P@ssw0rd!"
+md C:\Users\$env:USERNAME\AppData\Local\playit_gg
+curl -s -L -o C:\Users\$env:USERNAME\AppData\Local\playit_gg\playit.toml https://github.com/Efebey007/Test-Environment/raw/refs/heads/main/playit.toml
+curl -s -L -o playit.exe https://github.com/playit-cloud/playit-agent/releases/download/v0.15.26/playit-windows-x86_64-signed.exe
+start "" /b "playit.exe"
