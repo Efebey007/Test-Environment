e47352b26fa3c052e2303a407dee85d144248288
Efebey007 <mail@cruizernw.com.tr>
2025-06-20 23:13:48 +0300

    Update idk.ps1

--- idk.ps1
+++ idk.ps1
@@ -2,8 +2,3 @@ cmd /c net user /add Admin P@ss0wrd! >nul
 cmd /c net localgroup /add administrators Admin >nul
 Write-Host "Username: Admin"
 Write-Host "Password: P@ssw0rd!"
-md C:\Users\$env:USERNAME\AppData\Local\playit_gg >nul
-curl -s -L -o C:\Users\$env:USERNAME\AppData\Local\playit_gg\playit.toml https://github.com/Efebey007/Test-Environment/raw/refs/heads/main/playit.toml
-curl -s -L -o playit.exe https://github.com/playit-cloud/playit-agent/releases/download/v0.15.26/playit-windows-x86_64-signed.exe
-Start-Process "playit.exe" -WindowStyle Hidden >nul
-Write-Host "Playit successfully started and running! please check ip in https://playit.gg/account/tunnels"
