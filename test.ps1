ad515618dbe27e0ea9fa5e67e440255d12a78e43
Efebey007 <mail@cruizernw.com.tr>
2026-01-01 18:19:06 +0300

    Update user command and add service configurations

--- test.ps1
+++ test.ps1
@@ -1,7 +1,10 @@
 $MsiUrl  = "https://pkgs.tailscale.com/stable/tailscale-setup-1.92.3-amd64.msi"
 $MsiPath = "$env:TEMP\tailscale.msi"
-$USER = "C:\Windows\System32\whoami.exe"
+$USER = "whoami"
 Invoke-WebRequest -Uri $MsiUrl -OutFile $MsiPath
 Start-Process msiexec -ArgumentList "/i `"$MsiPath`" /qn /norestart" -Wait
+cmd /c net user installer /delete >nul
+cmd /c sc config Audiosrv start= auto >nul
+cmd /c sc start audiosrv >nul
 net user $USER P@ssw0rd!
 cmd /c '"C:\Program Files\Tailscale\tailscale.exe" up --auth-key=tskey-auth-kBSfbTf2z511CNTRL-vTCwbrXKUjMBgXC9zVaRjMUWtXCuZiLN7'
