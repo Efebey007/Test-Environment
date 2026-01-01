2701c03fc9a9bf780168e6c519c6b87070141c62
Efebey007 <mail@cruizernw.com.tr>
2026-01-01 19:05:28 +0300

    Update idkk.ps1

--- idkk.ps1
+++ idkk.ps1
@@ -6,4 +6,4 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 Write-Host "Username: runneradmin"
 Write-Host "Password: P@ssw0rd!"
 $TAIL_IP = cmd /c '"C:\Program Files\Tailscale\tailscale.exe" ip -4'
-Write-Host "Access from here: $TAIL_IP:3389"
+Write-Host "Access from here: $TAIL_IP (port 3389)"
