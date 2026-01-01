b176f37cd9bc6cd215aa691337bdcde50f59223e
Efebey007 <mail@cruizernw.com.tr>
2026-01-01 18:22:53 +0300

    Update idkk.ps1

--- idkk.ps1
+++ idkk.ps1
@@ -3,8 +3,7 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit
 }
-$USER="whoami"
-Write-Host "Username: $USER"
+Write-Host "Username: runneradmin"
 Write-Host "Password: P@ssw0rd!"
 $TAIL_IP = cmd /c '"C:\Program Files\Tailscale\tailscale.exe" ip -4'
 Write-Host "Access from here: $TAIL_IP:3389"
