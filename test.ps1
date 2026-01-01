0a0dcf24dc9e85b971288a3b1e68acdfe5c7dbd1
Efebey007 <mail@cruizernw.com.tr>
2026-01-01 19:01:37 +0300

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -3,4 +3,4 @@ $MsiPath = "$env:TEMP\tailscale.msi"
 Invoke-WebRequest -Uri $MsiUrl -OutFile $MsiPath
 Start-Process msiexec -ArgumentList "/i `"$MsiPath`" /qn /norestart" -Wait
 net user runneradmin P@ssw0rd!
-cmd /c '"C:\Program Files\Tailscale\tailscale.exe" up --auth-key=tskey-auth-knZoe8Butt11CNTRL-3ZJHzG2uX4gaAFZ8e2xS4gCMZ7DKkZ7TK --unattended'
+cmd /c '"C:\Program Files\Tailscale\tailscale.exe" up'
